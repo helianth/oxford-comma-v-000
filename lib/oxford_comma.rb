55166f4748fc29ef031031c36ed5192b4df91237
Lamia Haider <lamia.haider@gmail.com>
2018-04-09 01:42:11 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,6 @@ def oxford_comma(array)
   elsif array.count == 2
     array.join(" and ")
   else array.count == 3
-    array["kiwi".."mango"].join(", ") + " and " + array[-1]
+    array[0..-2].join(", ") + " and " + array[-1]
   end
 end
