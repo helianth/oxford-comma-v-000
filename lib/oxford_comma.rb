ed2f16698dd7a4d7ac63857d1f490d747198d1e4
Lamia Haider <lamia.haider@gmail.com>
2018-04-09 01:22:10 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,9 @@
 def oxford_comma(array)
   if array.count == 1
     array.join
-  else array.count == 2
+  elsif array.count == 2
     array.join(" and ")
+  elsif array.count == 3
+    array.join(", ")
   end
 end
